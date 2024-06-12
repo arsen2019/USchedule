7bad405c642ff0a3dbfaaffa24690c39ae47da85
arsen2019 <arsen.grigoryan555@gmail.com>
2024-06-12 13:18:18 +0400

    Add methods to get session and adjust the Base model

---
+++ core/models/__init__.py
@@ -0,0 +1,17 @@
+__all__ = (
+    "Base",
+    "Teacher",
+    "Building",
+    "Room",
+    "Department",
+    "Major",
+    "Group",
+    "Student",
+    "Course",
+    "DatabaseHelper",
+    "db_helper",
+)
+
+from .base import Base
+from .db_helper import db_helper, DatabaseHelper
+from .models import Teacher, Building, Room, Department, Major, Group, Student, Course
