c610239e74866c9ee7ff03ca97afab1ac9779f4c
arsen2019 <arsen.grigoryan555@gmail.com>
2024-07-15 11:02:21 +0400

    Add environment variable

--- core/config.py
+++ core/config.py
@@ -1,12 +1,14 @@
 from sqlalchemy.engine import URL
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
 
+# Load .env file
+load_dotenv()
 
 class Settings(BaseSettings):
     api_v1_prefix: str = ""
-    db_url: str = (
-        f"postgresql+asyncpg://postgres:Postgresql123!@localhost:5432/postgres"
-    )
+    db_url: str = os.getenv("DB_URL")
     db_echo: bool = True
 
 
