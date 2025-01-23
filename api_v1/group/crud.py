a935f992eeb078e7e5f5030b6fe955e287c9297a
Arsen Grigoryan <arsen.grigoryan555@gmail.com>
2025-01-23 20:49:49 +0400

    Trigerring Action trying new things3

--- api_v1/group/crud.py
+++ api_v1/group/crud.py
@@ -12,9 +12,8 @@ async def get_group(session: AsyncSession):
 
     if not group:
         raise HTTPException(status_code=404, detail="group not found")
-
-    return group
     print("Davo is the best")
+    return group
 
 async def get_lab(session: AsyncSession):
     stmt = select(Lab)
