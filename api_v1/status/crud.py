df5d814ffc1f60aa47f6f2c9bb9059bd14d5c165
Arsen Grigoryan <arsen.grigoryan555@gmail.com>
2025-07-21 00:00:55 +0400

    Testing CI/CD pipline

--- api_v1/status/crud.py
+++ api_v1/status/crud.py
@@ -1,3 +1,3 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 async def status(session:AsyncSession):
-    return {"status": "Everything is fine, I forgot that CI/CD triggers with tags"}
+    return {"status": "Everything is fine"}
